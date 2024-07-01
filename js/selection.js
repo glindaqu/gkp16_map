a7c2248dc8f7db2b078e07f46be9b3c72ad18a07
glindaqu <q5481996@gmail.com>
2024-07-01 19:28:18 +0700

    bugfix

--- js/selection.js
+++ js/selection.js
@@ -7,7 +7,9 @@ let x = 0, y = 0;
 let startLatlng = null, endLatlng = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-    mapManager.map.on("mousedown", e => startLatlng = e.latlng);
+    mapManager.map.on("mousedown", e => {
+        startLatlng = e.latlng;
+    });
     mapManager.map.on("mouseup", e => {
         endLatlng = e.latlng;
         filterJsonWithSelection(startLatlng, endLatlng);
@@ -17,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("contextmenu", e => e.preventDefault());
 
 document.addEventListener("mousedown", e => {
-    assert(e.button == 2);
+    if (e.button != 2) return;
     border.classList.remove("hidden");
     x = e.clientX;
     y = e.clientY;
@@ -26,12 +28,14 @@ document.addEventListener("mousedown", e => {
     mapManager.map.dragging.disable();
 });
 
-document.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", e => {
+    if (e.button != 2) return;
     border.classList.add("hidden");
     mapManager.map.dragging.enable();
 });
 
 document.addEventListener("mousemove", e => {
+    // if (e.button != 2) return;
     let minX = Math.min(x, e.clientX);
     let maxX = Math.max(x, e.clientX);
     let minY = Math.min(y, e.clientY);
